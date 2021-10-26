ce00a104687af56d969580329bc656475329fe02
Dmitri Gordeev <dimapushkin@gmail.com>
2021-10-26 12:52:33 +0300

    Initial Commit

---
+++ TrafficLightWithSwiftUI/TrafficLightWithSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrafficLightWithSwiftUIApp.swift
+//  TrafficLightWithSwiftUI
+//
+//  Created by Повелитель on 26.10.2021.
+//
+
+import SwiftUI
+
+@main
+struct TrafficLightWithSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
